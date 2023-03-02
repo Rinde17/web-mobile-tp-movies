0f26eeec53b76949490a167970e20a1fd13abaac
Rinde17 <perrot.calvin@outlook.fr>
2023-03-02 10:28:03 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'web-mobile-tp-movies-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'web-mobile-tp-movies';
+}
