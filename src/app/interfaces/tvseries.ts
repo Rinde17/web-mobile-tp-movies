965afb6fef79bd3dd8e0303a7e6d88c05994e47e
BastienBenech <bastbenech@gmail.com>
2023-03-10 18:18:32 +0100

    add infos for tv series details

--- src/app/interfaces/tvseries.ts
+++ src/app/interfaces/tvseries.ts
@@ -14,4 +14,22 @@ export interface ITvSerie {
   original_language: string;
   name: string;
   original_name: string;
+  number_of_seasons: number;
+  created_by: [{
+    id: number;
+    credit_id: string;
+    name: string;
+    gender: number;
+    profile_path: string|null;
+  }]
+  genres: [{
+    id: number;
+    name: string;
+  }]
+  networks: [{
+    name: string;
+    id: number;
+    logo_path: string;
+    origin_country: string;
+  }]
 }
