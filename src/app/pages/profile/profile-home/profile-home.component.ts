0c3df28f712b939d24e826be69b4744f43d0eccf
Rinde17 <perrot.calvin@outlook.fr>
2023-03-16 11:02:22 +0100

    test err

--- src/app/pages/profile/profile-home/profile-home.component.ts
+++ src/app/pages/profile/profile-home/profile-home.component.ts
@@ -1,44 +1,29 @@
-import {Component, OnInit} from '@angular/core';
-import {Subject, takeUntil} from 'rxjs';
-import {DataStorageService} from 'src/app/services/data-storage/data-storage.service';
-import {AuthService} from 'src/app/services/auth/auth.service';
-import {getAuth} from '@angular/fire/auth';
+import { Component, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { DataStorageService } from 'src/app/services/data-storage/data-storage.service';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { getAuth } from '@angular/fire/auth';
 
 @Component({
-    selector: 'web-mobile-tp-movies-profile-home',
-    templateUrl: './profile-home.component.html',
-    styleUrls: ['./profile-home.component.scss'],
+  selector: 'web-mobile-tp-movies-profile-home',
+  templateUrl: './profile-home.component.html',
+  styleUrls: ['./profile-home.component.scss'],
 })
 export class ProfileHomeComponent implements OnInit {
-    infos: any;
-    destroyed$: Subject<boolean> = new Subject<boolean>();
+  infosProfile: any;
+  destroyed$: Subject<boolean> = new Subject<boolean>();
 
-    constructor(
-        private _dataStorageService: DataStorageService,
-        private _authService: AuthService
-    ) {
-    }
-
-    ngOnInit(): void {
-        this._authService.userState
-            .pipe(takeUntil(this.destroyed$))
-            .subscribe(() => {
-                const authUser = getAuth().currentUser;
-                this.infos = this._authService.getInfos(authUser?.email);
-            });
-    }
-    // this._authService.userState
-    //   .pipe(takeUntil(this.destroyed$))
-    //   .subscribe(() => {
-    //     const authUser = getAuth().currentUser;
-    //     this.infos = this._dataStorageService.getInfos(authUser?.email).snapshotChanges().pipe(
-    //       map(changes =>
-    //           changes.map(c =>
-    //               ({ first_name: c.payload.doc.data() })
-    //           )
-    //       )
-    //     ).subscribe();
-    //   });
-    //
-    //   console.log(this.infos);
+  constructor(
+    private _dataStorageService: DataStorageService,
+    private _authService: AuthService
+  ) {}
+  ngOnInit(): void {
+    this._authService.userState
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe(() => {
+        const authUser = getAuth().currentUser;
+        this.infosProfile = this._authService.getInfos(authUser?.email);
+      });
+  }
 }
+//
--- src/app/services/data-storage/data-storage.service.ts
+++ src/app/services/data-storage/data-storage.service.ts
@@ -1,94 +1,89 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {AngularFireAuth} from '@angular/fire/compat/auth';
-import {AuthService} from '../auth/auth.service';
-import {AngularFirestore} from '@angular/fire/compat/firestore';
-import {IMovie} from 'src/app/interfaces/movie';
-import {ITvSerie} from 'src/app/interfaces/tvseries';
-import {isMovieTypeGuard} from 'src/app/interfaces/person';
-import {IFirestoreMedia} from 'src/app/interfaces/firestore';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AuthService } from '../auth/auth.service';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { IMovie } from 'src/app/interfaces/movie';
+import { ITvSerie } from 'src/app/interfaces/tvseries';
+import { isMovieTypeGuard } from 'src/app/interfaces/person';
+import { IFirestoreMedia } from 'src/app/interfaces/firestore';
 import 'firebase/firestore';
-import {AngularFireDatabase} from "@angular/fire/compat/database";
-import {map} from "rxjs/operators";
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 export class DataStorageService {
+  constructor(
+    private httpClient: HttpClient,
+    private authService: AuthService,
+    private angularFireAuth: AngularFireAuth,
+    private db: AngularFireDatabase,
+    private angularFirestore: AngularFirestore
+  ) {}
 
-    constructor(
-        private httpClient: HttpClient,
-        private authService: AuthService,
-        private angularFireAuth: AngularFireAuth,
-        private db: AngularFireDatabase,
-        private angularFirestore: AngularFirestore,
-    ) {
-    }
+  addMediaToWatchlist(
+    media: IMovie | ITvSerie,
+    userEmail: string,
+    callback: (error?: string) => void
+  ): void {
+    const mediaDetails: IFirestoreMedia = {
+      id: media.id,
+      createdAt: new Date(),
+      title: isMovieTypeGuard(media) ? media.title : media.original_name,
+      posterPath: media.poster_path,
+      isWatched: false,
+      mediaType: isMovieTypeGuard(media) ? 'movie' : 'tv-show',
+    };
 
-    addMediaToWatchlist(
-        media: IMovie | ITvSerie,
-        userEmail: string,
-        callback: (error?: string) => void
-    ): void {
-        const mediaDetails: IFirestoreMedia = {
-            id: media.id,
-            createdAt: new Date(),
-            title: isMovieTypeGuard(media) ? media.title : media.original_name,
-            posterPath: media.poster_path,
-            isWatched: false,
-            mediaType: isMovieTypeGuard(media) ? 'movie' : 'tv-show',
-        };
+    this.angularFirestore
+      .doc(`Users/${userEmail}`)
+      .collection<IFirestoreMedia[]>('watchlist')
+      .doc<IFirestoreMedia>(`${media.id}`)
+      .set(mediaDetails)
+      .then((success) => callback())
+      .catch((error) => callback(error));
+  }
 
-        this.angularFirestore
-            .doc(`Lists/${userEmail}`)
-            .collection<IFirestoreMedia[]>('watchlist')
-            .doc<IFirestoreMedia>(`${media.id}`)
-            .set(mediaDetails)
-            .then((success) => callback())
-            .catch((error) => callback(error));
-    }
+  addMediaToFavorite(
+    media: IMovie | ITvSerie,
+    userEmail: string,
+    callback: (error?: string) => void
+  ): void {
+    const mediaDetails: IFirestoreMedia = {
+      id: media.id,
+      createdAt: new Date(),
+      title: isMovieTypeGuard(media) ? media.title : media.original_name,
+      posterPath: media.poster_path,
+      isWatched: false,
+      mediaType: isMovieTypeGuard(media) ? 'movie' : 'tv-show',
+    };
 
-    addMediaToFavorite(
-        media: IMovie | ITvSerie,
-        userEmail: string,
-        callback: (error?: string) => void
-    ): void {
-        const mediaDetails: IFirestoreMedia = {
-            id: media.id,
-            createdAt: new Date(),
-            title: isMovieTypeGuard(media) ? media.title : media.original_name,
-            posterPath: media.poster_path,
-            isWatched: false,
-            mediaType: isMovieTypeGuard(media) ? 'movie' : 'tv-show',
-        };
+    this.angularFirestore
+      .doc(`Users/${userEmail}`)
+      .collection<IFirestoreMedia[]>('favorite')
+      .doc<IFirestoreMedia>(`${media.id}`)
+      .set(mediaDetails)
+      .then((success) => callback())
+      .catch((error) => callback(error));
+  }
 
-        this.angularFirestore
-            .doc(`Lists/${userEmail}`)
-            .collection<IFirestoreMedia[]>('favorite')
-            .doc<IFirestoreMedia>(`${media.id}`)
-            .set(mediaDetails)
-            .then((success) => callback())
-            .catch((error) => callback(error));
-    }
+  getFavorites(userEmail: string) {
+    const listFavorites = [];
+    this.angularFirestore
+      .collection(`Users/${userEmail}/favorite`)
+      .get()
+      .toPromise()
+      .then((querySnapshot) => {
+        querySnapshot.docs.forEach((doc) => {
+          listFavorites.push(doc.data());
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    return listFavorites;
+  }
 
-    getFavorites(userEmail: string) {
-        const listFavorites = [];
-        this.angularFirestore
-            .collection(`Lists/${userEmail}/favorite`)
-            .get()
-            .toPromise()
-            .then((querySnapshot) => {
-                querySnapshot.docs.forEach((doc) => {
-                    listFavorites.push(doc.data());
-                    console.log(listFavorites);
-                });
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-
-        return listFavorites;
-    }
+  //
 }
-
-
