81100f23db44abe4734e33c9417f86bb0d889ddc
Théodore Diouf <92777850+Tdiouf10@users.noreply.github.com>
2023-03-17 11:58:50 +0100

    update password and data profile

--- src/app/pages/profile/profile-home/profile-home.component.ts
+++ src/app/pages/profile/profile-home/profile-home.component.ts
@@ -1,27 +1,78 @@
-import { Component, OnInit } from '@angular/core';
-import { Subject, takeUntil } from 'rxjs';
-import { DataStorageService } from 'src/app/services/data-storage/data-storage.service';
-import { AuthService } from 'src/app/services/auth/auth.service';
-import { getAuth } from '@angular/fire/auth';
+import {Component, OnInit} from '@angular/core';
+import {Subject, takeUntil} from 'rxjs';
+import {AuthService} from 'src/app/services/auth/auth.service';
+import {getAuth, updateEmail, updatePassword, user} from '@angular/fire/auth';
+import {AngularFirestore} from "@angular/fire/compat/firestore";
+import {AngularFireAuth} from "@angular/fire/compat/auth";
+import * as authActions from "../../auth/auth-store/auth.actions";
+import {Store} from "@ngrx/store";
+import {IAuthState} from "../../auth/auth-store";
+import {Router} from "@angular/router";
 
 @Component({
-  selector: 'web-mobile-tp-movies-profile-home',
-  templateUrl: './profile-home.component.html',
-  styleUrls: ['./profile-home.component.scss'],
+    selector: 'web-mobile-tp-movies-profile-home',
+    templateUrl: './profile-home.component.html',
+    styleUrls: ['./profile-home.component.scss'],
 })
 export class ProfileHomeComponent implements OnInit {
-  infosProfile: any;
-  destroyed$: Subject<boolean> = new Subject<boolean>();
+    infosProfile: any;
+    email: string;
+    password: string;
+    success: string;
+    destroyed$: Subject<boolean> = new Subject<boolean>();
 
-  constructor(
-    private _authService: AuthService
-  ) {}
-  ngOnInit(): void {
-    this._authService.userState
-      .pipe(takeUntil(this.destroyed$))
-      .subscribe(() => {
-        const authUser = getAuth().currentUser;
-        this.infosProfile = this._authService.getInfos(authUser?.email);
-      });
-  }
+    constructor(
+        private _authService: AuthService, private angularFirestore: AngularFirestore,
+        private _angularFireAuth: AngularFireAuth,
+        private _router: Router,
+        private store: Store<IAuthState>
+    ) {
+        this._authService.userState.subscribe((user) => {
+            this.email = user?.email;
+            this.password = user?.password;
+        });
+    }
+
+    ngOnInit(): void {
+        this._authService.userState
+            .pipe(takeUntil(this.destroyed$))
+            .subscribe(() => {
+                const authUser = getAuth().currentUser;
+                this.infosProfile = this._authService.getInfos(authUser?.email);
+            });
+    }
+
+    updateProfile() {
+        this.angularFirestore.collection('Users').doc(this.email).update({
+            first_name: this.infosProfile.first_name,
+            last_name: this.infosProfile.last_name,
+        }).then(() => {
+            this.success = 'Données mises à jour';
+            window.location.reload();
+        });
+    }
+
+    // updatePassword(password: string) {
+    //     return this._angularFireAuth.currentUser.then((user) => {
+    //         user.updatePassword(password).then(() => {
+    //             this.store.dispatch(authActions.logOut());
+    //             this._router.navigate(['/auth']);
+    //             console.log('Password updated');
+    //         }).catch((error) => {
+    //             console.log(error);
+    //         });
+    //     });
+    // }
+
+    updatePassword2(newPassword: string) {
+        const auth = getAuth();
+        const user = auth.currentUser;
+        updatePassword(user, newPassword).then(() => {
+            this.store.dispatch(authActions.logOut());
+            this._router.navigate(['/auth']);
+            console.log('Password updated');
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
 }
