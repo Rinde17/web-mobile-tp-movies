abf3d12fcbae90876419bef4e0619287874cf75f
Théodore Diouf <92777850+Tdiouf10@users.noreply.github.com>
2023-03-23 09:47:59 +0100

    #5 first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,52 +1,60 @@
-import { Store } from '@ngrx/store';
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from './services/auth/auth.service';
-import { Router } from '@angular/router';
-import { IUser } from './interfaces/user';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
-import { IAuthState } from './pages/auth/auth-store';
+import {Store} from '@ngrx/store';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from './services/auth/auth.service';
+import {Router} from '@angular/router';
+import {IUser} from './interfaces/user';
+import {Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
+import {IAuthState} from './pages/auth/auth-store';
 import * as authActions from './pages/auth/auth-store/auth.actions';
 
 @Component({
-  selector: 'web-mobile-tp-movies-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'web-mobile-tp-movies-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  isShown = false;
+    isShown = false;
+    infosProfile: any;
+    user: IUser;
+    destroyed$: Subject<boolean> = new Subject<boolean>();
 
-  user: IUser;
+    constructor(
+        private _authService: AuthService,
+        private _router: Router,
+        private store: Store<IAuthState>
+    ) {
+    }
 
-  destroyed$: Subject<boolean> = new Subject<boolean>();
+    ngOnInit() {
+        this._authService.userState
+            .pipe(takeUntil(this.destroyed$))
+            .subscribe((response) => {
+                this.user = response
+                this.infosProfile = this.getInfos(this.user?.email);
+            })
+        ;
+    }
 
-  constructor(
-    private _authService: AuthService,
-    private _router: Router,
-    private store: Store<IAuthState>
-  ) {}
+    logOut(): void {
+        this.store.dispatch(authActions.logOut());
+        this._router.navigate(['/auth']);
+    }
 
-  ngOnInit() {
-    this._authService.userState
-      .pipe(takeUntil(this.destroyed$))
-      .subscribe((response) => (this.user = response));
-  }
+    logIn(): void {
+        this._router.navigate(['/auth']);
+    }
 
-  logOut(): void {
-    this.store.dispatch(authActions.logOut());
-    this._router.navigate(['/auth']);
-  }
+    profile(): void {
+        this._router.navigate(['/profile']);
+    }
 
-  logIn(): void {
-    this._router.navigate(['/auth']);
-  }
+    ngOnDestroy(): void {
+        this.destroyed$.next(true);
+        this.destroyed$.complete();
+    }
 
-  profile(): void {
-    this._router.navigate(['/profile']);
-  }
-
-  ngOnDestroy(): void {
-    this.destroyed$.next(true);
-    this.destroyed$.complete();
-  }
+    getInfos(email: string) {
+        return this._authService.getInfos(email);
+    }
 }
