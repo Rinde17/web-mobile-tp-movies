d349b6002408e5777c03f77db262467e6bafa5d9
Théodore Diouf <92777850+Tdiouf10@users.noreply.github.com>
2023-04-07 09:40:57 +0200

    :hammer: people detail

--- src/app/pages/people/people-list-item/people-list-item.component.ts
+++ src/app/pages/people/people-list-item/people-list-item.component.ts
@@ -1,38 +1,36 @@
-import { CommonModule } from '@angular/common';
-import { Component, OnInit, Input } from '@angular/core';
-import { IPerson, isMovieTypeGuard } from 'src/app/interfaces/person';
-import { Router } from '@angular/router';
+import {Component, Input, OnInit} from '@angular/core';
+import {IPerson, isMovieTypeGuard} from 'src/app/interfaces/person';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'web-mobile-tp-movies-people-list-item',
-  templateUrl: './people-list-item.component.html',
-  styleUrls: ['./people-list-item.component.scss'],
+    selector: 'web-mobile-tp-movies-people-list-item',
+    templateUrl: './people-list-item.component.html',
+    styleUrls: ['./people-list-item.component.scss'],
 })
 export class PeopleListItemComponent implements OnInit {
-  @Input() person: IPerson | undefined;
+    @Input() person: IPerson | undefined;
 
-  constructor(private _router: Router) {}
-
-  ngOnInit(): void {
-    this.formattedKnownFor();
-  }
-
-  // Iterate thru the known for property and check if it's movie or a tv show so we can display it without errors.
-  formattedKnownFor() {
-    if (this.person) {
-      return this.person.known_for
-        .map((item) => {
-          return isMovieTypeGuard(item) ? item.title : item.name;
-        })
-        .join(', ');
+    constructor(private _router: Router) {
     }
-    return null;
-  }
 
-  redirect(): void {
-    if (this.person) {
-      this._router.navigate(['/people', this.person.id]);
+    ngOnInit(): void {
+        this.formattedKnownFor();
     }
-    this._router.navigate(['/home']);
-  }
+
+    // Iterate thru the known for property and check if it's movie or a tv show so we can display it without errors.
+    formattedKnownFor() {
+        if (this.person) {
+            return this.person.known_for
+                .map((item) => {
+                    return isMovieTypeGuard(item) ? item.title : item.name;
+                })
+                .join(', ');
+        }
+        return null;
+    }
+
+    redirect(): void {
+      const id = this.person?.id;
+      this._router.navigate(['/people', id]);
+    }
 }
